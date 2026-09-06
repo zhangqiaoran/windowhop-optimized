d15fb9da561e7ed9492707faf61761abf0d6ca90
zhangqiaoran <liuzijiang_work@163.com>
2026-09-06 11:14:00 +0800

    docs(v2.3): point website at 2.3 release

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,11 +1,11 @@
 const WINDOWHOP_SITE = Object.freeze({
-  version: "2.2.0",
+  version: "2.3.0",
   website: "https://zhangqiaoran.github.io/my-alt-tab/",
   github: "https://github.com/zhangqiaoran/my-alt-tab",
   issues: "https://github.com/zhangqiaoran/my-alt-tab/issues",
   releases: "https://github.com/zhangqiaoran/my-alt-tab/releases",
-  releaseNotes: "https://github.com/zhangqiaoran/my-alt-tab/releases/tag/v2.2.0",
-  download: "https://github.com/zhangqiaoran/my-alt-tab/releases/latest/download/my-alt-tab-2.2.0.zip",
+  releaseNotes: "https://github.com/zhangqiaoran/my-alt-tab/releases/tag/v2.3.0",
+  download: "https://github.com/zhangqiaoran/my-alt-tab/releases/latest/download/my-alt-tab-2.3.0.zip",
   license: "https://github.com/zhangqiaoran/my-alt-tab/blob/main/LICENSE",
   altTab: "https://github.com/lwouis/alt-tab-macos",
 });
